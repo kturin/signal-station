6a9f7bf9c49bc3c438234f47315c2c4eafe37f52
Konrad Koziol <kturin@pm.me>
2023-12-10 06:38:39 +0100

    feat: More type safety, some explaining and test cases, for now in demo

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,34 +1,80 @@
-import {makeSignalStation, SignalStation} from "../src/signal-station";
+import {createSignalStation} from "../src/signal-station";
 
 console.log('Demo cases loaded');
 
-type testSignalType = {
-    testSignal: string,
+type testSignalSetType = {
+    testSignal1: string,
     testSignal2: number,
+    testSignal3: void,
 };
-const testSignalStation:SignalStation<testSignalType> = makeSignalStation('testSignal', 'testSignal2');
 
-testSignalStation.testSignal.subscribe((payload) => {
+// typed station
+const testSignalStation = createSignalStation<testSignalSetType>('testSignal1', 'testSignal2', 'testSignal3');
+
+// const testSignalStation: Signals<testSignalSetType> = createSignalStation('testSignal1', 'testSignal2', 'testSignal3'); // alternatively
+
+
+// untyped station
+const noTypeDefinedSignalStation = createSignalStation('testUntypedSignal');
+
+
+// subscribe: good typed station cases
+testSignalStation.testSignal1.subscribe((payload) => {  // okay, payload is string as intended
     console.log('testSignal received:', payload);
+    payload.toUpperCase();
 });
-
-testSignalStation.testSignal2.subscribe((payload) => {
+testSignalStation.testSignal2.subscribe((payload) => { //okay, payload is number as intended
     console.log('testSignal2 received:', payload);
+    Math.sqrt(payload);
+});
+testSignalStation.testSignal3.subscribe(() => { // okay, no payload as intended
+    console.log('testSignal2 received:');
+});
+testSignalStation.testSignal2.subscribe(() => { // okay, you can subscribe to signal with payload with an empty callback, that's not forbidden
+    console.log('testSignal2 received:');
 });
 
-testSignalStation.testSignal.publish('testPayload');
-testSignalStation.testSignal2.publish(1234);
-// testSignalStation.testSignal.publish(1234); // error, wrong payload type
 
+// subscribe: bad typed station cases
+// testSignalStation.testSignal3.subscribe((payload) => { // error, no payload allowed in testSignal3 type
+//     console.log('testSignal3 received', payload);
+// });
+// testSignalStation.testSignal1.subscribe((payload1, payload2) => { // error, multiple payloads are never allowed
+//     console.log('testNoDefSignal received with multiple payloads (this shall never happen):', payload1, payload2);
+// });
+// testSignalStation.testSignal1.subscribe((payload) => { // error, no square-rooting strings.
+//     Math.sqrt(payload);
+// });
 
-// testSignalStation.wrongSignalName.publish('testPayload'); // error, wrong signal name
-
-const noTypeDefinedSignalStation = makeSignalStation('testUntypedSignal');
-
-noTypeDefinedSignalStation.testUntypedSignal.subscribe((payload) => {
+// subscribe: good untyped station cases
+noTypeDefinedSignalStation.testUntypedSignal.subscribe((payload) => { // okay, payload is any as intended
     console.log('testNoDefSignal received:', payload);
 });
+noTypeDefinedSignalStation.testUntypedSignal.subscribe(() => { // okay, payload is any as intended and no payload is allowed
+    console.log('testNoDefSignal received with no payload');
+});
 
-noTypeDefinedSignalStation.testUntypedSignal.publish('testPayload');
-noTypeDefinedSignalStation.testUntypedSignal.publish(1234);
+// subscribe: bad untyped station cases
+// noTypeDefinedSignalStation.testUntypedSignal.subscribe((payload1, payload2) => { // wrong, payload is any as intended and multiple payloads are allowed
+//     console.log('testNoDefSignal received with multiple payloads (this shall never happen):', payload1, payload2);
+// });
+
+// publish: good typed station cases
+testSignalStation.testSignal1.publish('testPayload');   // okay, payload is string as intended
+testSignalStation.testSignal2.publish(1234);            // okay, payload is number as intended
+testSignalStation.testSignal3.publish(); // okay, "undefined" is allowed because testsignal3 has void type in testSignalSetType
+
+// publish: bad typed station cases
+// testSignalStation.testSignal1.publish(1234); // error, wrong payload type
+// testSignalStation.testSignal1.publish('testPayload1', 'testPayload2'); // error, multiple payload arguments are never allowed
+// testSignalStation.testSignal2.publish(); // error, wrong payload type ("undefined" when should be "number")
+// testSignalStation.wrongSignalName.publish('testPayload'); // error, wrong signal name
+
+// publish: good untyped station cases
+noTypeDefinedSignalStation.testUntypedSignal.publish('testPayload'); // okay, payload is string as intended
+noTypeDefinedSignalStation.testUntypedSignal.publish(1234);  // okay, payload is number as intended
+noTypeDefinedSignalStation.testUntypedSignal.publish(); // okay, "undefined" is allowed when no type is defined for whole signal station
+
+// publish: bad untyped station cases
+// noTypeDefinedSignalStation.testUntypedSignal.publish('testPayload1', 'testPayload2'); // error, multiple payload arguments are never allowed
 // noTypeDefinedSignalStation.wrongSignalName.publish('testPayload'); // error, wrong signal name
--- src/signal-station.ts
+++ src/signal-station.ts
@@ -4,22 +4,57 @@
  *
  * Design choices:
  * - Signal names and payload types are defined on construction via type generics and constructor arguments.
- * - Payload is a single argument (or undefined). If you need to send more, use an object or an array.
- * - Subscriber callbacks are called immediately with no timeout or deferral.
+ * - Payload is always a single argument only if defined. If you need to send more, use an object, array, map, set, whatever you fancy.
+ * - Subscribe and publish without payload is supported, for that truly stateless experience.
+ * - Subscriber callbacks are called immediately with no timeout or deferral. (may reconsider this)
  */
 
 
-type SignalCallback<payloadType> = (signalPayload: payloadType) => void;
-type SubscribeFunction<payloadType> = (onSignal: SignalCallback<payloadType>) => Subscriber<payloadType>;
-type UnsubscribeFunction<payloadType> = (subscription: Subscriber<payloadType>) => void;
-type PublishFunction<payloadType> = (payload: payloadType) => void;
+// Helper for determining if a type is specifically "any" or not. Don't ask why and how it works, unless you don't value your sanity.
+type IsAny<Match> = (Match extends never ? true : false) extends false ? false : true;
 
+
+/** Defines allowed callback signatures.
+ * If callback type is defined as void, then no payload is allowed.
+ * If callback type is defined as any, then any payload is allowed, and it is optional. (This happens when we create a station without providing a signals collection type.)
+ * If callback type is defined as a specific type, then only that type is allowed, and it is mandatory.
+ */
+type SignalCallback<PayloadType> =
+    IsAny<PayloadType> extends true
+        ? (payload?: any) => void
+        : PayloadType extends void
+            ? () => void
+            : (payload: PayloadType) => void;
+
+/**
+ * Defines allowed subscribe function signatures.
+ * Subscribe function always takes a single callback function as an argument, and returns a subscriber object, which can be used later to unsubscribe, or for debugging.
+ */
+type SubscribeFunction<PayloadType> = (onSignal: SignalCallback<PayloadType>) => Subscriber<PayloadType>;
+
+/**
+ * Defines allowed unsubscribe function signatures.
+ * Unsubscribe function always takes a subscriber object that was returned from Subscribe function as an argument.
+ */
+type UnsubscribeFunction<PayloadType> = (subscription: Subscriber<PayloadType>) => void;
+
+/**
+ * Defines allowed publish function signatures.
+ * Publish function always takes a single payload argument, and sends it to all subscribers.
+ * If payload type is defined as void, then no payload is allowed.
+ * If payload type is defined as any, then any payload is allowed, and it is optional. (This happens when we create a station without providing a signals collection type.)
+ * If payload type is defined as a specific type, then only that type is allowed, and it is mandatory.
+ */
+type PublishFunction<PayloadType> = IsAny<PayloadType> extends true ? (payload?: any) => void : (payload: PayloadType) => void;
+
+// Wrapper around a callback function, contains metadata and minimal state needed for executing signals once.
 type Subscriber<payloadType> = {
     signalCallback: SignalCallback<payloadType>;
     signalName: string | number | symbol;
     singleSignalOnly?: boolean;
 }
 
+// Signal, AKA Topic or Event. Main exposed API structure.
 type Signal<payloadType> = {
     subscribe: SubscribeFunction<payloadType>;
     unsubscribe: UnsubscribeFunction<payloadType>;
@@ -27,34 +62,50 @@ type Signal<payloadType> = {
     subscribeOnce: SubscribeFunction<payloadType>;
 }
 
-type DefaultSignalDefinition = {
-    [signalName: string | number | symbol]: any;
+// Default signal collection type, used when no type is provided on station creation.
+type DefaultSignalTypesCollection = {
+    [SignalName: string | number | symbol]: any;
 };
 
-
-export type SignalStation<SignalDefinition> = {
-    [SignalName in keyof SignalDefinition]: Signal<SignalDefinition[SignalName]>
+// Collection of signals. May seem redundant, but it's useful for checking and validating externally, hence the export.
+export type Signals<SignalTypesCollection = DefaultSignalTypesCollection> = {
+    [SignalName in keyof SignalTypesCollection]: Signal<IsAny<SignalTypesCollection[SignalName]> extends true ? any : SignalTypesCollection[SignalName]>
 };
 
-export function makeSignalStation<SignalDefinition = DefaultSignalDefinition>(
-    ...signalNames: (keyof SignalDefinition)[]
+/**
+ * Creates a signal station.
+ * @param signalNames - Names of signals to create. Must be unique strings, numbers or symbols.
+ * @generic SignalTypesCollection - Collection of signals to create. Must be a type with keys matching signalNames, and values matching payload types.
+ * @returns Signal station object, including all signals and station-level methods.
+ */
+export function createSignalStation<SignalsCollectionType>(
+    ...signalNames: (keyof SignalsCollectionType)[]
 ) {
 
-    const signals: SignalStation<SignalDefinition> = {} as SignalStation<SignalDefinition>;
+    const signals: Signals<SignalsCollectionType> = {} as Signals<SignalsCollectionType>;
 
+    // Populate on construction. Signals are immutable, we can't add them later.
     for (const signalName of signalNames) {
-        if (signals[signalName]){
+        if (signals[signalName]) {
             throw new Error(`Signal name ${String(signalName)} is already defined. Use unique signal names (or symbols with the same description if you feel adventurous).`);
         }
-        signals[signalName] = registerSignal<SignalDefinition[keyof SignalDefinition]>(signalName);
+        signals[signalName] = registerSignal(signalName);
     }
 
-    function registerSignal<payloadType = SignalDefinition[keyof SignalDefinition]>(signalName: keyof SignalDefinition): Signal<payloadType> {
+    // Shorthand for determining the payload type for unique signals, mapped type magic based on provided SignalsCollectionType. Without provided SignalsCollectionType, it defaults to "any".
+    type PayloadType = IsAny<SignalsCollectionType[keyof SignalsCollectionType]> extends true ? any : SignalsCollectionType[keyof SignalsCollectionType];
 
-        const subscribers: Subscriber<payloadType>[] = [];
 
-        const subscribe: SubscribeFunction<payloadType> = (signalCallback) => {
-            const subscriber: Subscriber<payloadType> = {
+    function registerSignal(signalName: keyof SignalsCollectionType): Signal<PayloadType> {
+
+        const subscribers: Subscriber<PayloadType>[] = [];
+
+        /**
+         * Subscribes to a signal. Callback is called when a signal is published.
+         * @param signalCallback - Callback function to be called when signal is published.
+         */
+        const subscribe: SubscribeFunction<PayloadType> = (signalCallback) => {
+            const subscriber: Subscriber<PayloadType> = {
                 signalCallback,
                 signalName: signalName
             };
@@ -62,20 +113,32 @@ export function makeSignalStation<SignalDefinition = DefaultSignalDefinition>(
             return subscriber;
         };
 
-        const subscribeOnce: SubscribeFunction<payloadType> = (onSignal) => {
-            const singleSubscriber = subscribe(onSignal);
+        /**
+         * Subscribes to a signal, but only once. After the first signal is received, the subscription is automatically removed.
+         * @param signalCallback - Callback function to be called when signal is published.
+         */
+        const subscribeOnce: SubscribeFunction<PayloadType> = (signalCallback: SignalCallback<PayloadType>) => {
+            const singleSubscriber = subscribe(signalCallback);
             singleSubscriber.singleSignalOnly = true;
             return singleSubscriber;
         };
 
-        const unsubscribe: UnsubscribeFunction<payloadType> = (subscriber) => {
+        /**
+         * Unsubscribes from a signal. Callback passed to given subscriber will no longer be called when signal is published.
+         * @param subscriber - Subscriber object returned from subscribe() or subscribeOnce() function.
+         */
+        const unsubscribe: UnsubscribeFunction<PayloadType> = (subscriber: Subscriber<PayloadType>): void => {
             const indexToDelete = subscribers.indexOf(subscriber);
             subscribers.splice(indexToDelete, 1);
         };
 
-        const publish: PublishFunction<payloadType> = (signalPayload: payloadType) => {
+        /**
+         * Publishes a signal. All subscribers are called with the provided payload.
+         * @param payload - Payload to send to all registered subscribers.
+         */
+        const publish: PublishFunction<PayloadType> = (payload: PayloadType): void => {
             for (const subscriber of subscribers) {
-                subscriber.signalCallback(signalPayload);
+                subscriber.signalCallback(payload);
                 if (subscriber.singleSignalOnly) {
                     unsubscribe(subscriber);
                 }
@@ -92,6 +155,7 @@ export function makeSignalStation<SignalDefinition = DefaultSignalDefinition>(
 
     return {
         ...signals
+        // any future station-level methods go here, so don't go overzealous with simplifying, please.
     };
 
 }
